a65a53ca0e367f412f166a395a9e9b9bdb886d07
Krak9n <alyoshagnatu@gmail.com>
2025-07-25 15:25:48 +0200

    install

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing..."
-git clone https://github.com/Krak9n/dice_roller.git
-cd dice_roller
 make
-<<<<<<< HEAD
 sudo make install
-=======
 make install
->>>>>>> a6ae12f (few changes)
 echo "Installation complete!"
